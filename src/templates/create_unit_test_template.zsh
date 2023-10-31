c261aea18389bb56b27f762cab06ba2bb6ee9957
wuchuheng <root@wuchuheng.com>
2023-10-31 21:26:21 +0800

    refactory: make the create_unit_test_template.zsh more readability.

--- src/templates/create_unit_test_template.zsh
+++ src/templates/create_unit_test_template.zsh
@@ -46,7 +46,7 @@ readonly UNIT_TEST_FILE=${BASE_DIR}/${max_number}_${file_name_without_extend}.te
 cat > ${UNIT_TEST_FILE} << EOF 
 #!/usr/bin/env zsh
 
-import @/src/handlers/testing_callback_handler/testing_callback_handler.zsh
+import @/src/handlers/testing_callback_handler/testing_callback_handler.zsh # {testing_callback_handle}
 import @/src/utils/test_except.zsh # {except_str}
 
 # the demo testing
