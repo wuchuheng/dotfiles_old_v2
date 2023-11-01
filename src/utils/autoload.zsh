9d97618fb6e3a2e9fbf2f52b30c3952c67db1b94
wuchuheng <root@wuchuheng.com>
2023-11-01 16:22:13 +0800

    refactor: make a error message more friendly in import function

--- src/utils/autoload.zsh
+++ src/utils/autoload.zsh
@@ -8,6 +8,58 @@ if [[ -z ${_all_imported_source_files} ]]; then
   declare -g -A _all_imported_source_files=()
 fi
 
+##
+# @desc 获取一个文件1-10行内容
+# @use  get_a_part_of_code "/Users/wuchuheng/dotfiles/tmp.sh" 5
+# @return
+#
+#    1  | readonly ALL_UNIT_TEST_FILES=(
+#    2  | utils/__test__/unit_tests/1_helper.test.sh
+#    3  | utils/__test__/unit_tests/2_helper.test.sh
+#    4  | utils/__test__/unit_tests/3_helper.test.sh
+# -> 5  | utils/__test__/unit_tests/4_helper.test.sh
+#    6  | utils/__test__/unit_tests/5_helper.test.sh
+#    7  | utils/__test__/unit_tests/6_helper.test.sh
+#    8  | utils/__test__/unit_tests/7_helper.test.sh
+#    9  | utils/__test__/unit_tests/8_helper.test.sh
+#    10 | utils/__test__/unit_tests/9_helper.test.sh
+#    11 | utils/__test__/unit_tests/10_helper.test.sh
+#
+##
+function get_a_part_of_code() {
+  local file=$1
+  local lineNumber=$2;
+  local allLine=$(wc -l ${file} | awk '{print $1}')
+  local intervalWidth=5 # 区间宽度
+  local intervalStart=$lineNumber # 区间开始坐标
+  local intervalEnd=$lineNumber # 区间结始坐标
+  for (( i=1; i <= 5; i++ )); do
+    if ((((intervalStart - 1)) > 0)) ; then
+      ((intervalStart--))
+    else
+      ((intervalEnd++))
+    fi
+    if (( ((intervalEnd + 1)) <= ${allLine} )); then
+      ((intervalEnd++))
+    else
+      if ((((intervalStart - 1)) > 0)) ; then
+        ((intervalStart--))
+      fi
+    fi
+  done
+  local result=`sed -n "${intervalStart},${intervalEnd}p" $file`
+  local cln=${intervalStart}
+  local lineNumberWidth=${#intervalEnd}
+  while IFS= read -r line; do
+    if (( cln == lineNumber )); then
+      printf " \033[0;31m\033[1m->\e[0m %-${lineNumberWidth}s | $line\n" $cln
+    else
+      printf "    %-${lineNumberWidth}s | $line\n" $cln
+    fi
+    ((cln++))
+  done <<< "$result"
+}
+
 function import() {
   local RED='\033[0;31m'
   local NC='\033[0m'
@@ -44,14 +96,25 @@ function import() {
   esac
   ## to check the source_file is exist or not in _all_imported_source_files
   if [[ ! -k _all_imported_source_files[$source_file] ]]; then
-    source "$source_file";
-    if [[ $? != 0 ]]; then
-      IFS=' ' read -r -a parts <<< $(caller 0)
-      printf "${RED}✖ ${timestamp} %s: line %s: %s ${NC} \n" ${parts[2]} ${parts[0]} "Failed to load $source_file, the file not found."
+    if [[ ! -f ${source_file} ]]; then
+      local prevFileLine="${funcfiletrace[1]}"
+      local prevFile=''
+      local prevLine=''
+      for (( i=${#prevFileLine}; i >= 0; i-- )); do
+        if [[ ${prevFileLine:$i:1} == ':' ]]; then
+          prevFile=${prevFileLine:0:$i}
+          break
+        else
+          prevLine=${prevFileLine:$i:1}${prevLine}
+        fi
+      done
+      printf "${RED}✖ Failed to load ${source_file}, the file not found in ${prevFileLine} ${NC} \n"
+      get_a_part_of_code "${prevFile}" "${prevLine}"
       exit 1
-      else
-        _all_imported_source_files[${source_file}]='loaded'
     fi
+
+    source "$source_file";
+    _all_imported_source_files[${source_file}]='loaded'
   fi
 }
 
