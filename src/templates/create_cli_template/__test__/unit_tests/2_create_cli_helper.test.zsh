787701b2efe902163b26307cd04fa4178823167e
wuchuheng <root@wuchuheng.com>
2023-11-01 20:41:28 +0800

    test: update create_cli_helper.test

--- src/templates/create_cli_template/__test__/unit_tests/2_create_cli_helper.test.zsh
+++ src/templates/create_cli_template/__test__/unit_tests/2_create_cli_helper.test.zsh
@@ -9,7 +9,7 @@ function get_cli_installation_checker_provider_file_path_test() {
   globalCliInstallationCheckerProviderFilePath=''
   get_cli_installation_checker_provider_file_path "1_tmp_cli" globalCliInstallationCheckerProviderFilePath
   local exceptStrValue="src/cli/1_tmp_cli/tmp_cli_installation_checker_provider/tmp_cli_installation_checker_provider.zsh"
-  except_str "${exceptStrValue}" "${globalCliInstallationCheckerProviderFilePath}"
+  except_str "${exceptStrValue}" "${globalCliInstallationCheckerProviderFilePath:${#APP_BASE_PATH + 1}}"
 }
 
 testing_callback_handle "get_cli_installation_checker_provider_file_path_test" "Unit test src/templates/create_cli_template/__test__/unit_tests/2_create_cli_helper.test.zsh"
