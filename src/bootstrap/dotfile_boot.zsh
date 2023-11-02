ca1239e17fee2682286cb62e37fce311e2bd64a8
wuchuheng <root@wuchuheng.com>
2023-11-02 20:54:24 +0800

    refactory(boot): make the bootloader file more readability.

--- src/bootstrap/dotfile_boot.zsh
+++ src/bootstrap/dotfile_boot.zsh
@@ -4,9 +4,9 @@
 
 source ${APP_BASE_PATH}/src/utils/autoload.zsh
 
-import ../utils/cli_helper.zsh # {get_cli_name_by_number_cli_dir}
 import ../utils/ref_variable_helper.zsh # {generate_unique_var_name, get_str_from_ref}
-import @/src/templates/create_cli_template/create_cli_helper.zsh #{get_cli_installation_provider_file_path, get_cli_boot_file_path}
+import @/src/templates/create_cli_template/create_cli_helper.zsh #{get_cli_uninstallation_provider_file_path}
+import ./boot_helper.zsh # { check_cli_by_number_dir }
 import @/src/utils/load_env.zsh # {set_env_type}
 set_env_type 'prod'
 
@@ -17,25 +17,23 @@ local cliDirList=($(get_str_from_ref "${cliDirListRef}"));
 
 # to trigger the installation provider from cli dir
 for numberCliDirName in "${cliDirList[@]}"; do
-  local cliInstallationCheckerProviderFilePathRef=$(generate_unique_var_name)
-  get_cli_installation_checker_provider_file_path "${numberCliDirName}" "${cliInstallationCheckerProviderFilePathRef}"
-  local cliInstallationCheckerProviderFilePath=$(get_str_from_ref "${cliInstallationCheckerProviderFilePathRef}")
-  import "${cliInstallationCheckerProviderFilePath}"
-
-  # get cli name without number.
-  local cliNameRef=$(generate_unique_var_name)
-  get_cli_name_by_number_cli_dir "${numberCliDirName}" "${cliNameRef}"
-  local cliName=$(get_str_from_ref "${cliNameRef}")
-
-  # trigger the installation checker.
-  ${cliName}_installation_checker
+  # check the cli is installation or not.
+  check_cli_by_number_dir "${numberCliDirName}"
   local isInstallation=$?
+
   # if the cli was not installed, then install it.
   if [[ "${isInstallation}" -eq "${TRUE}" ]]; then
     local cliBootFilePathRef=$(generate_unique_var_name)
     get_cli_boot_file_path "${numberCliDirName}" "${cliBootFilePathRef}"
     local cliBootFilePath=$(get_str_from_ref "${cliBootFilePathRef}")
+
+    # import the bootloader file for cli.
     import "${cliBootFilePath}"
+
+    # get cli name without number.
+    local cliNameRef=$(generate_unique_var_name)
+    get_cli_name_by_number_cli_dir "${numberCliDirName}" "${cliNameRef}"
+    local cliName=$(get_str_from_ref "${cliNameRef}")
 
     # trigger the cli boot provider.
     ${cliName}_boot
--- src/bootstrap/zsh_install_boot.zsh
+++ src/bootstrap/zsh_install_boot.zsh
@@ -10,9 +10,9 @@
 # Declare a global variable in zsh while runtime for APP_PATH
 typeset -g APP_BASE_PATH=$(pwd); source "${APP_BASE_PATH}"/src/utils/autoload.zsh || exit 1
 
-import ../utils/cli_helper.zsh # {get_cli_name_by_number_cli_dir}
 import ../utils/ref_variable_helper.zsh # {generate_unique_var_name, get_str_from_ref}
-import @/src/templates/create_cli_template/create_cli_helper.zsh #{get_cli_installation_provider_file_path}
+import @/src/templates/create_cli_template/create_cli_helper.zsh #{get_cli_uninstallation_provider_file_path}
+import ./boot_helper.zsh # { check_cli_by_number_dir }
 import @/src/utils/load_env.zsh # {set_env_type}
 set_env_type 'install'
 
@@ -23,25 +23,22 @@ local cliDirList=($(get_str_from_ref "${cliDirListRef}"));
 
 # to trigger the installation provider from cli dir
 for numberCliDirName in "${cliDirList[@]}"; do
-  local cliInstallationCheckerProviderFilePathRef=$(generate_unique_var_name)
-  get_cli_installation_checker_provider_file_path "${numberCliDirName}" "${cliInstallationCheckerProviderFilePathRef}"
-  local cliInstallationCheckerProviderFilePath=$(get_str_from_ref "${cliInstallationCheckerProviderFilePathRef}")
-  import "${cliInstallationCheckerProviderFilePath}"
-
-  # get cli name without number.
-  local cliNameRef=$(generate_unique_var_name)
-  get_cli_name_by_number_cli_dir "${numberCliDirName}" "${cliNameRef}"
-  local cliName=$(get_str_from_ref "${cliNameRef}")
-
-  # trigger the installation checker.
-  ${cliName}_installation_checker
+  # check the cli is installation or not.
+  check_cli_by_number_dir "${numberCliDirName}"
   local isInstallation=$?
   # if the cli was not installed, then install it.
   if [[ "${isInstallation}" -eq "${FALSE}" ]]; then
     local cliInstallationProviderFilePathRef=$(generate_unique_var_name)
     get_cli_installation_provider_file_path "${numberCliDirName}" "${cliInstallationProviderFilePathRef}"
     local installProviderPath=$(get_str_from_ref "${cliInstallationProviderFilePathRef}")
+
+    # import the installation provider file.
     import "${installProviderPath}"
+
+    # get cli name without number.
+    local cliNameRef=$(generate_unique_var_name)
+    get_cli_name_by_number_cli_dir "${numberCliDirName}" "${cliNameRef}"
+    local cliName=$(get_str_from_ref "${cliNameRef}")
 
     # trigger the installation provider.
     ${cliName}_installation_provider
