6676457f41b9e1e16cd58513633d8568ee8a5d6d
wuchuheng <root@wuchuheng.com>
2023-11-03 15:41:56 +0800

    issue(output): find out a issue that the description outputing for unit test is incorrect while to prcess the unit test

--- src/templates/create_cli_template/__test__/unit_tests/2_create_cli_helper.test.zsh
+++ src/templates/create_cli_template/__test__/unit_tests/2_create_cli_helper.test.zsh
@@ -9,8 +9,10 @@ function get_cli_installation_checker_provider_file_path_test() {
   globalCliInstallationCheckerProviderFilePath=''
   get_cli_installation_checker_provider_file_path "1_tmp_cli" globalCliInstallationCheckerProviderFilePath
   local exceptStrValue="src/cli/1_tmp_cli/tmp_cli_installation_checker_provider/tmp_cli_installation_checker_provider.zsh"
-  except_str "${exceptStrValue}" "${globalCliInstallationCheckerProviderFilePath:${#APP_BASE_PATH + 1}}"
+  local receiveValue=${globalCliInstallationCheckerProviderFilePath:${#APP_BASE_PATH} + 1}
+  except_str "${exceptStrValue}"  "${receiveValue}"
 }
 
-testing_callback_handle "get_cli_installation_checker_provider_file_path_test" "Unit test src/templates/create_cli_template/__test__/unit_tests/2_create_cli_helper.test.zsh"
+# TODO(ISSUE): except the print log for description is 'test get_cli_installation_checker_provider_file_path_test' ,but 'get_cli_installation_checker_provider_file_path_test' only in the unit test output
+testing_callback_handle "get_cli_installation_checker_provider_file_path_test" "test get_cli_installation_checker_provider_file_path_test"
 
